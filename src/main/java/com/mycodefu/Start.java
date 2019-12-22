7abbf051099a0811eb2181b242a08698dc9d6b70
Luke Thompson <endeavour9@gmail.com>
2019-12-22 18:00:41 +1100

    added a panel to the frame so that we can have more objects on the window

--- src/main/java/com/mycodefu/Start.java
+++ src/main/java/com/mycodefu/Start.java
@@ -7,13 +7,16 @@ import java.net.InetAddress;
 public class Start {
     public static void main(String[] args) throws Throwable{
         Frame frame = new Frame("Lan Test-"+getCurrentIp());
+        JPanel panel = new JPanel();
+        frame.add(panel);
         frame.setSize(320, 240);
-
+//panel.setFocusable(true);
         JToggleButton toggleButton = new JToggleButton();
         toggleButton.setText("Toggle Me");
-        frame.add(toggleButton);
+        panel.add(toggleButton);
 
         frame.setVisible(true);
+        //panel.requestFocusInWindow();
     }
     private static String getCurrentIp() throws Throwable{
         InetAddress inetAddress = InetAddress.getLocalHost();
