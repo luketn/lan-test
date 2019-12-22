7ef2573abf6d9f4e45d859ce53603fe011274f27
Luke Thompson <endeavour9@gmail.com>
2019-12-22 17:49:13 +1100

    added the ip address of the current device to the name of the window

--- src/main/java/com/mycodefu/Start.java
+++ src/main/java/com/mycodefu/Start.java
@@ -2,10 +2,11 @@ package com.mycodefu;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.InetAddress;
 
 public class Start {
-    public static void main(String[] args) {
-        Frame frame = new Frame("Lan Test");
+    public static void main(String[] args) throws Throwable{
+        Frame frame = new Frame("Lan Test-"+getCurrentIp());
         frame.setSize(320, 240);
 
         JToggleButton toggleButton = new JToggleButton();
@@ -13,6 +14,10 @@ public class Start {
         frame.add(toggleButton);
 
         frame.setVisible(true);
+    }
+    private static String getCurrentIp() throws Throwable{
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        return inetAddress.getHostAddress();
     }
     private static void openConnection(){
 
