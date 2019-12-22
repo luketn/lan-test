cf754572b68fd24e6eb6698db3c98b3d73f36307
Luke Thompson <endeavour9@gmail.com>
2019-12-22 17:28:54 +1100

    simple window with a toggle button

---
+++ src/main/java/com/mycodefu/Start.java
@@ -0,0 +1,17 @@
+package com.mycodefu;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Start {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Lan Test");
+        frame.setSize(320, 240);
+
+        JToggleButton toggleButton = new JToggleButton();
+        toggleButton.setText("Toggle Me");
+        frame.add(toggleButton);
+
+        frame.setVisible(true);
+    }
+}
