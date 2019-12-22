b7aa5cf14efebd377f831d6d8f87114c60b84f68
Luke Thompson <endeavour9@gmail.com>
2019-12-23 09:59:32 +1100

    print network interfaces

--- src/main/java/com/mycodefu/Start.java
+++ src/main/java/com/mycodefu/Start.java
@@ -3,6 +3,10 @@ package com.mycodefu;
 import javax.swing.*;
 import java.awt.*;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Random;
 
 public class Start implements MessageListener {
@@ -35,7 +39,23 @@ public class Start implements MessageListener {
 
     private static String getCurrentIp() throws Throwable {
         InetAddress inetAddress = InetAddress.getLocalHost();
+
+        System.out.println("\nList of network interfaces:");
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
+
         return inetAddress.getHostAddress();
+    }
+
+    static void displayInterfaceInformation(NetworkInterface netint) {
+        System.out.printf("Display name: %s\n", netint.getDisplayName());
+        System.out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            System.out.printf("InetAddress: %s\n", inetAddress);
+        }
+        System.out.printf("\n");
     }
 
     private static int getRandomPort()  {
