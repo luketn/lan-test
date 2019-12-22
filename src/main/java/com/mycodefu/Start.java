a5ee92133759c816af345fc387f761943f9f3b02
Luke Thompson <endeavour9@gmail.com>
2019-12-22 18:19:57 +1100

    added the port and added it to the name of the window

--- src/main/java/com/mycodefu/Start.java
+++ src/main/java/com/mycodefu/Start.java
@@ -3,12 +3,14 @@ package com.mycodefu;
 import javax.swing.*;
 import java.awt.*;
 import java.net.InetAddress;
+import java.util.Random;
 
 public class Start implements MessageListener {
     private static LanListener lanListener;
 
     public static void main(String[] args) throws Throwable {
-        Frame frame = new Frame("Lan Test-" + getCurrentIp());
+        int port = getRandomPort();
+        Frame frame = new Frame("Lan Test-" + getCurrentIp()+":"+port);
         JPanel panel = new JPanel();
         frame.add(panel);
         frame.setSize(320, 240);
@@ -25,6 +27,11 @@ public class Start implements MessageListener {
     private static String getCurrentIp() throws Throwable {
         InetAddress inetAddress = InetAddress.getLocalHost();
         return inetAddress.getHostAddress();
+    }
+
+    private static int getRandomPort()  {
+        Random rand = new Random();
+        return rand.nextInt(15000)+50000;
     }
 
     @Override
